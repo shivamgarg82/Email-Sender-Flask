01011f4b86ce78e329786b3bb147c222275a6d62
Shivam Garg <ngarg82871@gmail.com>
2025-05-30 22:11:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from flask_mail import Mail,Message # pip install flask_mail
+
+app = Flask(__name__) #Initialise app
+
+
+# Config
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'ngarg82871@gmail.com'
+app.config['MAIL_PASSWORD'] = '' # On Allow less secure app in gmail manage settings
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app) # Instance
+
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+
+        msg = Message(subject, sender = 'ngarg82871@gmail.com' , recipients = [email])
+        msg.body = message
+        mail.send(msg)
+        print(email,subject,message)
+        return render_template('index.html', sent = True)
+    else:
+        return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
